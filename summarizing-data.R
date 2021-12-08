6ac372b8d2597abdf427726fd5a52604b6c8b697
Rod Morgado <rodrigo@zooft.company>
2021-12-08 08:23:32 -0800

    testing samples with and without replacement

--- summarizing-data.R
+++ summarizing-data.R
@@ -41,3 +41,18 @@ amir_deals %>%
 amir_deals %>%
   count(product) %>%
   mutate(prob = n / sum(n))
+
+# Set random seed to 31
+set.seed(31)
+
+# Sample 5 deals without replacement
+amir_deals %>%
+  sample_n(5, replace = FALSE)
+
+# Set random seed to 31
+set.seed(31)
+
+# Sample 5 deals with replacement
+amir_deals %>%
+  sample_n(5, replace = TRUE)
+
