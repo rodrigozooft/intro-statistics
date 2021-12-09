64c63a8b8f951ddfd8759903c69c2bbebec5198e
Rod Morgado <rodrigo@zooft.company>
2021-12-08 20:31:47 -0800

    working with samples mean to test the central limit theorem

--- normal-distribution.R
+++ normal-distribution.R
@@ -12,3 +12,43 @@ new_sales <- new_sales %>%
 new_sales %>%
   ggplot(aes(x = amount)) +
   geom_histogram(bins = 10)
+
+# Create a histogram of num_users
+amir_deals %>%
+    ggplot(aes(x = num_users)) + 
+    geom_histogram(bins = 10)
+
+# Set seed to 104
+set.seed(104)
+
+# Sample 20 num_users with replacement from amir_deals
+sample(amir_deals$num_users, 20, replace = TRUE) %>%
+  # Take mean
+  mean()
+
+# Set seed to 104
+set.seed(104)
+
+# Sample 20 num_users from amir_deals and take mean
+sample(amir_deals$num_users, size = 20, replace = TRUE) %>%
+  mean()
+
+# Repeat the above 100 times
+sample_means <- replicate(100, sample(amir_deals$num_users, size = 20, replace = TRUE) %>% mean())
+
+# Set seed to 104
+set.seed(104)
+
+# Sample 20 num_users from amir_deals and take mean
+sample(amir_deals$num_users, size = 20, replace = TRUE) %>%
+  mean()
+
+# Repeat the above 100 times
+sample_means <- replicate(100, sample(amir_deals$num_users, size = 20, replace = TRUE) %>% mean())
+
+# Create data frame for plotting
+samples <- data.frame(mean = sample_means)
+
+# Histogram of sample means
+ggplot(samples, aes(x = mean))+ 
+  geom_histogram(bins = 10)
